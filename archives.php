2a247f12aca3e9407f85f6738786ac619b1f7c86
unknown <cjprimer@gmail.com>
2023-12-14 09:08:08 +0800

    changed

--- archives.php
+++ archives.php
@@ -1,15 +1,18 @@
-<!DOCTYPE php>
-<php>
+<!DOCTYPE html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
     <style>
         body {
             margin: 0;
             padding: 0;
-            font-family: Arial, sans-serif;
-            background-image: url(images/ustp.jpg);
+            font-family: 'Arial', sans-serif;
+            background-image: url('images/ustp.jpg');
             background-repeat: no-repeat;
             background-size: cover;
+            color: #333;
         }
 
         .sidebar {
@@ -58,14 +61,19 @@
         }
 
         .content {
-            margin-left: 270px;
-            padding: 20px;
-        }
-
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 20px;
+    margin-left: 250px; /* Adjust the margin to match the width of your sidebar */
+}
         .navbar {
             background-color: #007bff;
             color: white;
             padding: 15px;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
         }
 
         .navbar select,
@@ -76,7 +84,7 @@
         }
 
         .box {
-            background-color: rgba(255, 255, 255, 0.2);
+            background-color: rgba(255, 255, 255, 0.8);
             border-style: ridge;
             padding: 20px;
             border-radius: 10px;
@@ -104,107 +112,134 @@
             background-color: #cc0000;
         }
 
-        #pieChart {
-            max-width: 100%;
-            height: auto;
-            margin: 10px;
+        table {
+    margin-top: 10px; /* Adjusted margin */
+    border-collapse: collapse;
+    width: 100%;
+    background-color: #fff;
+    box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+    margin-bottom: 20px; /* Added margin-bottom for spacing */
+}
+
+th, td {
+    padding: 10px; /* Reduced padding */
+    text-align: left;
+    border-bottom: 1px solid #ddd;
+}
+
+th {
+    background-color: #007bff;
+    color: white;
+}
+
+tbody tr:hover {
+    background-color: rgba(0, 123, 255, 0.1);
+}
+
+.delete-button {
+            background-color: #28a745;
+            color: white;
+            border: none;
+            padding: 8px 12px;
+            cursor: pointer;
+            border-radius: 5px;
+        }
+
+        .delete-button:hover {
+            background-color: red;
         }
     </style>
 </head>
 <body>
-<div class="sidebar">
+    <div class="sidebar">
         <a href="adminOffice.php">Manage Responses</a>
         <a href="feedbacks.php">Collect Feedbacks and Comments</a>
         <a href="surveyQuestionnaire.php">Manage Questionnaires</a>
         <a href="archives.php">Archives</a>
     </div>
+
     <div class="content">
-        <div class="navbar">
-            <!-- Your filter options here -->
-            <select id="filterMonth">
-                <option value="">Select Month</option>
-                <option value="January">January</option>
-                <option value="February">February</option>
-                <option value="March">March</option>
-                <option value="April">April </option>
-                <option value="May">May</option>
-                <option value="June">June</option>
-                <option value="July">July</option>
-                <option value="August">August</option>
-                <option value="September">September</option>
-                <option value="October">October</option>
-                <option value="November">November</option>
-                <option value="December">December</option>
-                <!-- Add more months here -->
-            </select>
-            <select id="filterYear">
-                <option value="">Select Year</option>
-                <option value="2024">2024</option>
-                <option value="2023">2023</option>
-                <option value="2022">2022</option>
-                <!-- Add more years here -->
-            </select>
-            <select id="filterType">
-                <option value="">Select Type</option>
-                <option value="Guidance Office">Guidance Office</option>
-                <option value="ICT Office">ICT Office</option>
-                <option value="Accreditation Office">Accreditation Office</option>
-                <option value="Admin Office">Admin Office</option>
-                <option value="Registrar">Registrar</option>
-            </select>
-           
-            <button id="generateButton" onclick="generatePieChart">Generate Pie Graph</button>
-        </div>
+        <h2>Survey Responses</h2>
 
-        <!-- Your survey response content here -->
-        <div class="box">
-            <h1>Survey Response 1</h1>
-        </div>
-        <div class="box">
-            <h2>Survey Response 2</h2>
-        </div>
+        <?php
+        // Database connection details
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "css_system";
 
-        
-        <canvas id="pieChart" width="200" height="100"></canvas>
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        // Fetch survey responses from the database
+        $sql = "SELECT * FROM surveys_archive";
+        $result = $conn->query($sql);
+
+        // Display the table if there are rows in the result
+        // Display the table if there are rows in the result
+if ($result->num_rows > 0) {
+    echo '<table>';
+    echo '<thead>';
+    echo '<tr>';
+    echo '<th>ID</th>';
+    echo '<th>Question 1</th>';
+    echo '<th>Question 2</th>';
+    echo '<th>Question 3</th>'; // Add this line for question3
+    echo '<th>Question 4</th>'; // Add this line for question4
+    echo '<th>Question 5</th>'; // Add this line for question5
+    echo '<th>Question 6</th>'; // Add this line for question6
+    echo '<th>Question 7</th>'; // Add this line for question7
+    echo '<th>Question 8</th>'; // Add this line for question8
+    echo '<th>Question 9</th>'; // Add this line for question9
+    echo '<th>Feedback</th>';
+    echo '<th>Created At</th>';
+    echo '<th>Office Type</th>';
+    echo '<th>Action</th>';
+    echo '</tr>';
+    echo '</thead>';
+    echo '<tbody>';
+
+    // Output data of each row
+    while ($row = $result->fetch_assoc()) {
+        echo '<tr>';
+        echo '<td>' . $row["id"] . '</td>';
+        echo '<td>' . $row["question1"] . '</td>';
+        echo '<td>' . $row["question2"] . '</td>';
+        echo '<td>' . $row["question3"] . '</td>';
+        echo '<td>' . $row["question4"] . '</td>';
+        echo '<td>' . $row["question5"] . '</td>';
+        echo '<td>' . $row["question6"] . '</td>';
+        echo '<td>' . $row["question7"] . '</td>';
+        echo '<td>' . $row["question8"] . '</td>';
+        echo '<td>' . $row["question9"] . '</td>';
+        // Add more cells for other questions as needed
+        echo '<td>' . $row["feedback"] . '</td>';
+        echo '<td>' . $row["created_at"] . '</td>';
+        echo '<td>' . $row["office_type"] . '</td>';
+        echo '<td><button class="delete-button" onclick="deleteResponse(' . $row["id"] . ')">Remove</button></td>';
+        echo '</tr>';
+    }
+
+    echo '</tbody>';
+    echo '</table>';
+} else {
+    echo 'No survey responses found.';
+}
+
+
+        $conn->close();
+        ?>
 
         <button class="logout-button" onclick="logout()">Logout</button>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
     <script>
-        // Data for the pie chart (example data)
-        const pieChartData = {
-            labels: ['Strongly Agree', 'Agree', 'Neutral' , 'Disagree' , 'Strongly Disagree'],
-            datasets: [{
-                data: [10, 10, 30, 10, 10], // Replace with your data
-                backgroundColor: ['green', 'aqua', 'white' , 'red' , 'black'], // Colors for the segments
-            }]
-        };
 
-        const pieChartOptions = {
-            responsive: true,
-        };
-
-        // Function to generate the pie chart
-        function generatePieChart() {
-            const pieChartCanvas = document.getElementById('pieChart').getContext('2d');
-            new Chart(pieChartCanvas, {
-                type: 'pie',
-                data: pieChartData,
-                options: pieChartOptions,
-                
-            });
-        }
-
-        // Add a click event to the "Generate" button
-        document.getElementById('generateButton').addEventListener('click', generatePieChart);
-
-        // Your existing functions here (e.g., archiveResponses)
-
-        function archiveResponses() {
-            alert("Responses has been archived.")
-        }
-        
-        
         function logout() {
     // Display a confirmation dialog
     var confirmLogout = window.confirm("Are you sure you want to log out?");
@@ -217,4 +252,4 @@
 }
     </script>
 </body>
-</php>
+</html>
